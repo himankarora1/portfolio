d525bbcbcfed56deef931e2e7dbe6b36f768dd1f
himankarora1 <himankarora100@gmail.com>
2026-07-18 01:22:30 -0400

    Split the difference on rooftop still vertical framing.

--- src/utils/artistMedia.js
+++ src/utils/artistMedia.js
@@ -47,7 +47,7 @@ export const artistMedia = {
         duration: 4500,
         scale: 1,
         // A bit lower than 52% so head/mic stay in frame (clip after this stays as-is)
-        objectPosition: 'center 26%',
+        objectPosition: 'center 28%',
       },
       {
         type: 'video',
